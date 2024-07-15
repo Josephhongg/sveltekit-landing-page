bb9136c0c494053e684fd8caf50c15d0652606c3
josephhongg <joseph.hong99@gmail.com>
2024-07-15 18:16:04 +1200

    feat: create Conversion component

--- landing-page-app/src/routes/+page.svelte
+++ landing-page-app/src/routes/+page.svelte
@@ -3,6 +3,7 @@
     import Hero from "../components/Hero.svelte";
     import Product from "../components/Product.svelte"
 	import Reviews from "../components/Reviews.svelte";
+	import Conversion from '../components/Conversion.svelte';
 </script>
 
 <main class="flex flex-col">
@@ -10,4 +11,5 @@
     <Product />
     <Reviews />
     <FAQs />
+    <Conversion />
 </main>
