4e9fc2d75ff0d4f70b069ff1e568b699b5605483
josephhongg <joseph.hong99@gmail.com>
2024-07-15 17:16:17 +1200

    refactor: ProductCard formatting changes

--- landing-page-app/src/components/ProductCard.svelte
+++ landing-page-app/src/components/ProductCard.svelte
@@ -4,7 +4,9 @@
 </script>
 
 <div class="grid grid-cols-1 md:grid-cols-2 gap-14 md:gap-14 lg:gap-20 text-base">
-	<div class={"flex flex-col gap-8 md:gap-10 pt-8 sm:pt-10" + (index % 2 === 1 ? " md:order-2" : " ")}>
+	<div
+		class={'flex flex-col gap-8 md:gap-10 pt-8 sm:pt-10' + (index % 2 === 1 ? ' md:order-2' : ' ')}
+	>
 		<h4
 			class="text-2xl sm:text-3xl md:text-4xl ma-w-[1000px] w-full font-medium relative after:absolute after:top-full after:left-0 after:w-1/5 after:h-1.5 after:mt-1 after:bg-slate-900"
 		>
@@ -29,14 +31,14 @@
 			</button>
 		</div>
 	</div>
-	<div class={"flex flex-col dropShadow overflow-hidden rounded-b-lg"}>
+	<div class={'flex flex-col dropShadow overflow-hidden rounded-b-lg'}>
 		<div class="rounded-t-xl h-8 sm:h-10 bg-white opacity-60 px-3 flex items-center gap-2">
-			{#each [1,2,3] as i}
+			{#each [1, 2, 3] as i}
 				<div class="rounded-full aspect-square w-2.5 sm:w-3 bg-indigo-300" />
 			{/each}
 		</div>
 		<div class="flex flex-col bg-white gap-4 flex-1">
-			<img src={productFeature.imgUrl} alt="product-img"/>
+			<img src={productFeature.imgUrl} alt="product-img" />
 		</div>
 	</div>
 </div>
